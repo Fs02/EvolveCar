92d04bca830a7d77c2ae2e74737acf91f8c7ee59
FS02 <surya.asriadie@gmail.com>
2015-12-13 04:16:17 +0700

    reorganize project

--- Assets/LineFollower.cs
+++ Assets/LineFollower.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class LineFollower : MonoBehaviour {
     public Utility.CatmullRomSpline spline;
-    public LineRenderer line;
 
     void Update()
     {
-        line.SetPosition(0, transform.position);
-        line.SetPosition(1, spline.GetClosestPoint(transform.position, 30));
+        float distance;
+        Debug.DrawLine(transform.position, spline.GetClosestPoint(transform.position, out distance, 30));
+        Debug.Log(distance);
     }
 }
---
+++ Assets/Scripts/Experiment1/Checkpoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EvolveCar.Experiment1
+{
+    public class Checkpoint : MonoBehaviour
+    {
+        public bool last = false;
+
+        void OnTriggerEnter(Collider other)
+        {
+            gameObject.SetActive(false);
+            LearnDirector.Instance.ReportCheckPoint(this, last);
+        }
+    }
+}
